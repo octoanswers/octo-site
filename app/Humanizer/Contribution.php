c34145b05c087bc7db42e46805922f0c91334201
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2018-06-04 22:35:04 +0500

    update contributors top on Q page

--- app/Humanizer/Contribution.php
+++ app/Humanizer/Contribution.php
@@ -5,43 +5,33 @@
  */
 class Contribution_Humanizer
 {
-    public function humanizeCount(int $n): string
+    public static function humanizeCount(int $count): string
     {
         if ($count < 0) {
             throw new Exception('Count param '.$count.' must be greater than or equal to 0', 1);
         }
 
         if ($count === 0) {
-            return $this->loc->t('wordform', 'no_contributors');
+            return '0';
         }
 
         $formatted = '';
 
-        if($n >= 1000 && $n < 1000000)
-        {
-            if($n%1000 === 0)
-            {
-                $formatted = ($n/1000);
-            }
-            else
-            {
-                $formatted = substr($n, 0, -3).'.'.substr($n, -3, -2);
+        if ($count >= 1000 && $count < 1000000) {
+            if ($count%1000 === 0) {
+                $formatted = ($count/1000);
+            } else {
+                $formatted = substr($count, 0, -3) . '.' . substr($count, -3, -2);
 
-                if(substr($formatted, -1, 1) === '0')
-                {
+                if (substr($formatted, -1, 1) === '0') {
                     $formatted = substr($formatted, 0, -2);
                 }
             }
 
-            $formatted.= 'k';
+            $formatted .= 'K';
+        } else {
+            $formatted = $count;
         }
-
-        // $wordforms = [
-        //     $this->loc->t('wordform', '1_contributor'),
-        //     $this->loc->t('wordform', '2_contributors'),
-        //     $this->loc->t('wordform', '5_contributors'),
-        // ];
-        // $count_wordform = $this->humanizeCount($count, $wordforms);
 
         return $formatted;
     }
