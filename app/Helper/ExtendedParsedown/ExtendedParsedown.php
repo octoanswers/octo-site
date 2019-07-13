5da1f91ebaeb04f2f313610b0aa87efc32d210ff
gomzyakov <alexander.gomzyakov@gmail.com>
2019-07-13 23:24:45 +0500

    Close #334 Inline links to category

--- app/Helper/ExtendedParsedown/ExtendedParsedown.php
+++ app/Helper/ExtendedParsedown/ExtendedParsedown.php
@@ -2,8 +2,6 @@
 
 class ExtendedParsedown extends Parsedown
 {
-    const CATEGORY_PATTERN = '/#([^\s\W]+)/u';
-
     protected $lang;
 
     public function __construct(string $lang)
@@ -12,32 +10,7 @@ class ExtendedParsedown extends Parsedown
             parent::__construct();
         }
         $this->lang = $lang;
-
-        // $this->InlineTypes['#'][]= 'CategoryMention';
-        // $this->inlineMarkerList .= '#';
     }
-
-    // protected function inlineCategoryMention($Excerpt)
-    // {
-    //     if (preg_match(self::CATEGORY_PATTERN, $Excerpt['context'], $matches)) {
-    //         $category = new Category();
-    //         $category->title = $matches[1];
-    //         $categoryURL = $category->getURL($this->lang);
-            
-    //         return [
-    //             'extent' => strlen($matches[0]),
-    //             'element' => [
-    //                 'name' => 'a',
-    //                 'text' => $matches[0],
-    //                 'attributes' => [
-    //                     'href' => $categoryURL,
-    //                     'title' => '#'.$category->title,
-    //                     'class' => 'inline-category',
-    //                 ],
-    //             ],
-    //         ];
-    //     }
-    // }
 
     /**
      * Overwrite methods from Parsedown
@@ -48,13 +21,23 @@ class ExtendedParsedown extends Parsedown
         $offsetAdjustment = 0;
 
         // Transform empty links [foo]() to [foo](foo)
-        $shortLinkPattern = '/\[(.+)\](\(\))/uU';
+        $short_link_pattern = '/\[(.+)\](\(\))/uU';
         $replacement = '[$1]($1)';
-        if (preg_match($shortLinkPattern, $excerpt['text'], $matches)) {
-            $excerpt['text'] = preg_replace($shortLinkPattern, $replacement, $excerpt['text']);
-            $excerpt['context'] = preg_replace($shortLinkPattern, $replacement, $excerpt['context']);
+        if (preg_match($short_link_pattern, $excerpt['text'], $matches)) {
+            $excerpt['text'] = preg_replace($short_link_pattern, $replacement, $excerpt['text']);
+            $excerpt['context'] = preg_replace($short_link_pattern, $replacement, $excerpt['context']);
+
+            $cat_cat_pattern = '/\[cat\:(.+)\]\(cat\:(.+)\)/uU';
+            if (preg_match('/\[cat\:(.+)\]\(cat\:(.+)\)/uU', $excerpt['text'], $cat_matches)) {
+                $excerpt['text'] = preg_replace($cat_cat_pattern, '[cat:$1]($1)', $excerpt['text']);
+                $excerpt['context'] = preg_replace($cat_cat_pattern, '[cat:$1]($1)', $excerpt['context']);
+            }
+
             // @NOTE Учитывая, что мы изменяем длинну строк, необходимо будет скорректировать отступ.
             $offsetAdjustment = strlen($matches[0]) - 4;
+            if (preg_match('/\[cat\:(.+)\]\((.+)\)/uU', $excerpt['text'], $cat_matches)) {
+                $offsetAdjustment = strlen($matches[0]) - 8;
+            }
         }
 
         // Proced default links like a [foo](What is foo?)
@@ -64,18 +47,31 @@ class ExtendedParsedown extends Parsedown
             $referencePart = $matches[2];
 
             $offset = strlen($matches[0]) - $offsetAdjustment;
-            
-            $question = Question_Model::initWithTitle($referencePart);
 
-            $element = [
-                'name' => 'a',
-                'handler' => 'line',
-                'text' => $bodyPart,
-                'attributes' => [
-                    'href' => $question->getURL($this->lang),
-                    'title' => $referencePart,
-                ],
-            ];
+            // Question-link or category-link?
+            if (preg_match('/cat\:(.+)$/uU', $bodyPart, $cat_matches)) {
+                $category = Category::initWithTitle($referencePart);
+                $element =  [
+                    'name' => 'a',
+                    'text' => $cat_matches[1],
+                    'attributes' => [
+                        'href' => $category->getURL($this->lang),
+                        'title' => $category->title,
+                        'class' => 'inline-category'
+                    ],
+                ];
+            } else {
+                $question = Question_Model::initWithTitle($referencePart);
+                $element = [
+                    'name' => 'a',
+                    'handler' => 'line',
+                    'text' => $bodyPart,
+                    'attributes' => [
+                        'href' => $question->getURL($this->lang),
+                        'title' => $referencePart
+                    ],
+                ];
+            }
 
             // Reference part started with HTTP/HTTPS
             if (filter_var($referencePart, FILTER_VALIDATE_URL)) {
@@ -94,18 +90,12 @@ class ExtendedParsedown extends Parsedown
 
         return;
     }
-        
 
     /**
      * Only H2-H6 header tags in Q-article body.
      */
     protected function blockHeader($Line)
     {
-        // maybe #topic, not header?
-        if (preg_match(self::CATEGORY_PATTERN, $Line['text'], $matches)) {
-            return;
-        }
-
         if (isset($Line['text'][1])) {
             $level = 1;
 
@@ -125,7 +115,7 @@ class ExtendedParsedown extends Parsedown
 
             $Block = array(
                 'element' => array(
-                    'name' => 'h'.$hDigit,
+                    'name' => 'h' . $hDigit,
                     'text' => $text,
                     'handler' => 'line',
                 ),
@@ -137,6 +127,6 @@ class ExtendedParsedown extends Parsedown
 
     protected function _isAnsweropediaURL($url)
     {
-        return preg_match('/^https:\/\/'.SITE_URL_NAME.'\.org/', $url);
+        return preg_match('/^https:\/\/' . SITE_URL_NAME . '\.org/', $url);
     }
 }
