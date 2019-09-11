730a12ee85b94fda3f4d4884ae91fd525ef25412
gomzyakov <alexander.gomzyakov@gmail.com>
2019-09-11 16:34:42 +0500

    Code coverage Trait/Model/User/Avatar

--- tests/Trait/Model/User/URL/get_avatar_URL_large/Test.php
+++ tests/Trait/Model/User/URL/get_avatar_URL_large/Test.php
@@ -2,12 +2,22 @@
 
 class Trait_Model_User_URL__get_avatar_URL_largeTest extends PHPUnit\Framework\TestCase
 {
-    public function test__Full_params()
+    public function test__Default_avatar()
     {
         $user = new User_Model();
         $user->id = 13;
         $user->name = 'Sasha';
 
         $this->assertEquals('https://avatars.answeropedia.org/avatars/user.png?size=400&name=Sasha', $user->get_avatar_URL_large());
+    }
+
+    public function test__Avatar_uploaded()
+    {
+        $user = new User_Model();
+        $user->id = 13;
+        $user->name = 'Sasha';
+        $user->is_avatar_uploaded = true;
+
+        $this->assertEquals('https://answeropedia.org/uploads/avatar/13_400.jpg', $user->get_avatar_URL_large());
     }
 }
--- tests/Trait/Model/User/URL/get_avatar_URL_small/Test.php
+++ tests/Trait/Model/User/URL/get_avatar_URL_small/Test.php
@@ -2,12 +2,22 @@
 
 class Trait_Model_User_URL__get_avatar_URL_smallTest extends PHPUnit\Framework\TestCase
 {
-    public function test__Full_params()
+    public function test__Default_avatar()
     {
         $user = new User_Model();
         $user->id = 13;
         $user->name = 'Sasha';
 
         $this->assertEquals('https://avatars.answeropedia.org/avatars/user.png?size=100&name=Sasha', $user->get_avatar_URL_small());
+    }
+
+    public function test__Avatar_uploaded()
+    {
+        $user = new User_Model();
+        $user->id = 13;
+        $user->name = 'Sasha';
+        $user->is_avatar_uploaded = true;
+
+        $this->assertEquals('https://answeropedia.org/uploads/avatar/13_100.jpg', $user->get_avatar_URL_small());
     }
 }
