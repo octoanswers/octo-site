c74e7472a071be8a045e86af436fb153a7b871bc
gomzyakov <alexander.gomzyakov@gmail.com>
2019-09-24 17:17:10 +0500

    Fix tests

--- tests/Query/Relations/CategoriesToQuestions/find_newest_for_category_with_ID/Test.php
+++ tests/Query/Relations/CategoriesToQuestions/find_newest_for_category_with_ID/Test.php
@@ -14,9 +14,9 @@ class Query_Relations_CategoriesToQuestions__find_newest_for_category_with_IDTes
         $this->assertEquals(58, $relations[0]->categoryID);
         $this->assertEquals(338, $relations[0]->questionID);
 
-        $this->assertEquals(12, $relations[9]->id);
+        $this->assertEquals(11, $relations[9]->id);
         $this->assertEquals(58, $relations[9]->categoryID);
-        $this->assertEquals(33, $relations[9]->questionID);
+        $this->assertEquals(1938, $relations[9]->questionID);
     }
 
     public function test__First_page()
@@ -29,24 +29,24 @@ class Query_Relations_CategoriesToQuestions__find_newest_for_category_with_IDTes
         $this->assertEquals(58, $relations[0]->categoryID);
         $this->assertEquals(338, $relations[0]->questionID);
 
-        $this->assertEquals(12, $relations[9]->id);
+        $this->assertEquals(11, $relations[9]->id);
         $this->assertEquals(58, $relations[9]->categoryID);
-        $this->assertEquals(33, $relations[9]->questionID);
+        $this->assertEquals(1938, $relations[9]->questionID);
     }
 
     public function test__Second_page()
     {
         $relations = (new \Query\Relations\CategoriesToQuestions('ru'))->find_newest_for_category_with_ID(58, 2);
 
-        $this->assertEquals(5, count($relations));
+        $this->assertEquals(4, count($relations));
 
-        $this->assertEquals(11, $relations[0]->id);
+        $this->assertEquals(10, $relations[0]->id);
         $this->assertEquals(58, $relations[0]->categoryID);
         $this->assertEquals(1938, $relations[0]->questionID);
 
-        $this->assertEquals(5, $relations[4]->id);
-        $this->assertEquals(58, $relations[4]->categoryID);
-        $this->assertEquals(161, $relations[4]->questionID);
+        $this->assertEquals(5, $relations[3]->id);
+        $this->assertEquals(58, $relations[3]->categoryID);
+        $this->assertEquals(161, $relations[3]->questionID);
     }
 
     public function test__Third_page()
