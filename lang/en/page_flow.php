0e7f7c212ada0058c2a30653e87e9ebcd325ea7a
gomzyakov <alexander.gomzyakov@gmail.com>
2019-10-10 16:38:35 +0500

    #464 Flow page

---
+++ lang/en/page_flow.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "add_to_friends" => "Add to friends",
+    "all_activities" => "Все события",
+    "editing_answers" => "Editing answers",
+    "new_categories" => "New categories",
+    "new_questions" => "Новые вопросы",
+    "note" => "Questions and answers flow on Answeropedia.",
+    "page_title" => "Flow"
+];
