a8515a810c579e5feec3c1676fbf4c052eae0ce5
gomzyakov <alexander.gomzyakov@gmail.com>
2019-10-10 17:49:54 +0500

    #464 Settings page

---
+++ lang/en/page_settings.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    "avatar__btn_upload" => "Upload avatar",
+    "avatar__note" => "It is advisable to use your real photo as an avatar - this causes more confidence among users.",
+    "avatar__title" => "Avatar",
+    "choose_file" => "Choose file",
+    "real_name_note" => "The real name causes more trust than @username. It will be shown next to your edits and in the Top authors.",
+    "real_name_title" => "Real name",
+    "save" => "Save",
+    "signature_note" => "Enter a signature describing your professional skills and knowledge.",
+    "signature_placeholder" => "For example: Professor of Psychology, entrepreneur, novice poet",
+    "signature_title" => "Signature",
+    "site_note" => "Enter the address of your website or page on the social network.",
+    "site_title" => "Site",
+    "title" => "Settings"
+];
