5ccf2aedc0f8a37929a7957bf2eb5cdd07fe0d38
gomzyakov <alexander.gomzyakov@gmail.com>
2019-10-10 11:14:41 +0500

    #464 Main page

--- lang/en/common.php
+++ lang/en/common.php
@@ -1,5 +1,22 @@
 <?php
 
 return [
-    'answeropedia' => 'ZZAnsweropedia',
+    'answeropedia' => 'Answeropedia',
+    'minutes_choice' => 'minute|minutes',
+    "other_languages" => [
+        "all_languages" => "All languages",
+        "english" => "English",
+        "russian" => "Russian",
+        "title" => "Languages"
+    ],
+    "sections" => [
+        "title" => "Sections",
+        "feed" => "Feed",
+        "how_it_works" => "How it works",
+        "popular_categories" => "Categories",
+        "questions" => "New questions",
+        "random_question" => "Random question",
+        "recent_changes" => "Recent changes",
+        "sandbox" => "Sandbox"
+    ],
 ];
--- lang/ru/common.php
+++ lang/ru/common.php
@@ -2,4 +2,21 @@
 
 return [
     'answeropedia' => 'Ансверопедия',
+    'minutes_choice' => 'минута|минуты|минут',
+    "other_languages" => [
+        "all_languages" => "Все языки",
+        "english" => "Английский",
+        "russian" => "Русский",
+        "title" => "На других языках"
+    ],
+    "sections" => [
+        "title" => "Разделы",
+        "feed" => "Подписки",
+        "how_it_works" => "Как это работает",
+        "popular_categories" => "Категории",
+        "questions" => "Новые вопросы",
+        "random_question" => "Случайный вопрос",
+        "recent_changes" => "Свежие правки",
+        "sandbox" => "Песочница"
+    ],
 ];
