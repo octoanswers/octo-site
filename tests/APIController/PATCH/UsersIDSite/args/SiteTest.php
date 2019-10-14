b6ea40c8e6170fc6356d3aeb92e832aa86bbb3b0
gomzyakov <alexander.gomzyakov@gmail.com>
2019-10-14 17:06:24 +0500

    #490 Rename /negative folders to /args

--- tests/APIController/PATCH/UsersIDSite/args/SiteTest.php
+++ tests/APIController/PATCH/UsersIDSite/args/SiteTest.php
@@ -1,10 +1,44 @@
 <?php
 
-namespace Tests\APIController\PATCH\UsersIDSite\Abnormal;
+namespace Tests\APIController\PATCH\UsersIDSite;
 
 class SiteTest extends \Test\TestCase\Frontend
 {
     protected $setUpDB = ['ru' => ['activities'], 'users' => ['users']];
+
+    public function test_IncorrectURL()
+    {
+        $uri = '/api/v1/ru/users/3/site.json?api_key=7d21ebdbec3d4e396043c96b6ab44a6e' . '&' . 'site=' . urlencode('xxx');
+
+        $request = $this->createRequest('PATCH', $uri);
+        $response = $this->request($request);
+        $response_body = (string) $response->getBody();
+
+        $expected_response = [
+            'error_code'    => 0,
+            'error_message' => 'User "site" property "xxx" must be a URL',
+        ];
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertEquals($expected_response, json_decode($response_body, true));
+    }
+
+    public function test_SiteNotSet()
+    {
+        $uri = '/api/v1/ru/users/3/site.json?api_key=7d21ebdbec3d4e396043c96b6ab44a6e' . '&' . 'foo_site=' . urlencode('https://answeropedia.org');
+
+        $request = $this->createRequest('PATCH', $uri);
+        $response = $this->request($request);
+        $response_body = (string) $response->getBody();
+
+        $expected_response = [
+            'error_code'    => 0,
+            'error_message' => 'User "site" property null must be a string',
+        ];
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertEquals($expected_response, json_decode($response_body, true));
+    }
 
     public function test__URLWithoutProtocol()
     {
