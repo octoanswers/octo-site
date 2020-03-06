96746b45dd7baae0785affcdd55fb861abc643c8
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2020-03-06 23:57:03 +0500

    Apply fixes from StyleCI (#588)

--- tests/_DB/data/users/users.php
+++ tests/_DB/data/users/users.php
@@ -1,7 +1,7 @@
 <?php
 
 $pdo->exec(
-  "INSERT INTO `users` (`u_id`, u_username, `u_name`, `u_email`, u_signature, u_site, `u_password_hash`, `u_api_key`, u_created_at) VALUES
+    "INSERT INTO `users` (`u_id`, u_username, `u_name`, `u_email`, u_signature, u_site, `u_password_hash`, `u_api_key`, u_created_at) VALUES
   (1, 'gomzyakov', 'Александр Гомзяков', 'alexander.gomzyakov@gmail.com', NULL, NULL, '$2a$10$91968b5d5b465b37e8630eRtP9A.XoWkOP3xVDv6zYpOdhLr/ToIW', '34b88c8f1ed16fdcc18d93667c886fcc', '2016-02-24 08:00:52'),
   (2, 'cheremshov', 'Сява Черемшов', 'syava@gmail.ru', NULL, NULL, '$2a$10$6f28824558c757fb675c4eLiXg4mEsZUoZGy8iE1k89mrynOn/QHG', '123ae73181eea730380ef0c225b50f68', '2016-02-11 16:03:50'),
   (3, 'ivan', 'Иван Коршунов', 'admin@answeropedia.org', 'Old signature', NULL, '$2a$10$302c3a67fccf3f2c3b7a3ejzYNi6hadZm3abINiiGlcT03Kau4XAO', '7d21ebdbec3d4e396043c96b6ab44a6e', '2016-03-19 06:47:41'),
