ad1559889e6397ec84bac8abdb7269ee99cf2d61
gomzyakov <alexander.gomzyakov@gmail.com>
2020-03-24 23:14:52 +0500

    Fix style in Model

--- app/Model/Model.php
+++ app/Model/Model.php
@@ -2,17 +2,37 @@
 
 namespace Model;
 
+use Exception;
+
+/**
+ * Class Model
+ *
+ * @property void foo_bar() <- for tests use
+ *
+ * @package Model
+ */
 abstract class Model
 {
-    // Force PHP to throw an error on undefined Models property
-
+    /**
+     * Force PHP to throw an error on undefined Models property
+     *
+     * @param $var
+     * @param $val
+     *
+     * @throws Exception
+     */
     public function __set($var, $val)
     {
-        throw new \Exception("Property \"$var\" doesn't exists and cannot be set.", 0);
+        throw new Exception("Property \"$var\" doesn't exists and cannot be set.", 0);
     }
 
+    /**
+     * @param $var
+     *
+     * @throws Exception
+     */
     public function __get($var)
     {
-        throw new \Exception("Property \"$var\" doesn't exists and cannot be get.", 0);
+        throw new Exception("Property \"$var\" doesn't exists and cannot be get.", 0);
     }
 }
--- tests/Model/Test.php
+++ tests/Model/Test.php
@@ -2,14 +2,20 @@
 
 namespace Test\Model;
 
-class Test extends \PHPUnit\Framework\TestCase
+use Model\Model;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group models
+ */
+class Test extends TestCase
 {
     protected $anonymous_class_from_abstract;
 
     protected function setUp(): void
     {
         // Create a new instance from the Abstract Model Class
-        $this->anonymous_class_from_abstract = new class() extends \Model\Model {
+        $this->anonymous_class_from_abstract = new class() extends Model {
             public $bar = 'bar!';
 
             // Just a sample public function that returns this anonymous instance
@@ -25,29 +31,30 @@ class Test extends \PHPUnit\Framework\TestCase
         $this->anonymous_class_from_abstract = null;
     }
 
-    public function test__Call_exists_method()
+    public function test_Call_exists_method()
     {
         $this->assertEquals('foo', $this->anonymous_class_from_abstract->get_foo());
     }
 
-    public function test__Get_exists_param()
+    public function test_Get_exists_param()
     {
         $this->assertEquals('bar!', $this->anonymous_class_from_abstract->bar);
     }
 
-    public function test__Get_doesnt_exists_param()
+    public function test_Get_doesnt_exists_param()
     {
         $this->expectExceptionMessage('Property "foo_bar" doesn\'t exists and cannot be get.');
-        $foo_bar = $this->anonymous_class_from_abstract->foo_bar;
+
+        $this->anonymous_class_from_abstract->foo_bar;
     }
 
-    public function test__Set_exists_param()
+    public function test_Set_exists_param()
     {
         $this->anonymous_class_from_abstract->bar = '123';
         $this->assertEquals('123', $this->anonymous_class_from_abstract->bar);
     }
 
-    public function test__Set_doesnt_exists_param()
+    public function test_Set_doesnt_exists_param()
     {
         $this->expectExceptionMessage('Property "foo_bar" doesn\'t exists and cannot be set.');
         $this->anonymous_class_from_abstract->foo_bar = '123';
